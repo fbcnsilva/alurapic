fee6fa8d3422b70a1ddbbd898e92ad4e25d829d0
Fabio Silva <fabio.silva@printi.com.br>
2020-08-05 23:36:42 -0300

    Creating a fucking button component _\,,/

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -5,7 +5,13 @@
     <ul class="lista-fotos">
       <li class="lista-fotos-item" v-for="foto in fotosComFiltro">
         <meu-painel :titulo="foto.titulo">
-          <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
+          <imagem-responsiva :url="foto.url" :titulo="foto.titulo" />
+          <meu-botao
+            tipo="button"
+            rotulo="Remover"
+            @botaoAtivado="remove($event, foto)"
+            :confirmacao="true"
+            estilo="perigo" />
         </meu-painel>
       </li>
     </ul>
@@ -16,12 +22,20 @@
 
 import Painel from './../shared/painel/Painel.vue';
 import ImagemResponsiva from './../shared/imagem-responsiva/ImagemResponsiva.vue';
+import Botao from './../shared/botao/Botao.vue';
 
 export default {
 
   components: {
     'meu-painel': Painel,
-    'imagem-responsiva': ImagemResponsiva
+    'imagem-responsiva': ImagemResponsiva,
+    'meu-botao': Botao
+  },
+
+  methods: {
+    remove(event, foto) {
+      alert(foto.titulo);
+    }
   },
 
   data() {
