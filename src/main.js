d3f1c6741bf64aa687fd318d7e09c3e7bb80ecf0
Fabio Silva <fabio.silva@printi.com.br>
2020-08-05 22:53:32 -0300

    Adds VueRouter

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router';
 import VueResource from 'vue-resource';
 
+import { routes } from './routes';
+
+Vue.use(VueRouter);
 Vue.use(VueResource);
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: routes
+});
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
